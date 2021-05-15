4203e3fb11c3c4d51ee99a6da17dd904d8914362
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-15 06:50:53 +0200

    Update index.js

--- Ornamental_Objectives/8 kyu - What's the real floor?/index.js
+++ Ornamental_Objectives/8 kyu - What's the real floor?/index.js
@@ -5,7 +5,8 @@ Americans are odd people: in their buildings, the first floor is actually the gr
 
 Write a function that given a floor in the american system returns the floor in the european system.
 
-With the 1st floor being replaced by the ground floor and the 13th floor being removed, the numbers move down to take their place. In case of above 13, they move down by two because there are two omitted numbers below them.
+With the 1st floor being replaced by the ground floor and the 13th floor being removed, the numbers move down to take their place. 
+In case of above 13, they move down by two because there are two omitted numbers below them.
 
 Basements (negatives) stay the same as the universal level.
 
