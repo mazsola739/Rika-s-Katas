7620a645fe4d8fd6cc0f7b0adf43d798ed3bb34b
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-15 10:08:31 +0200

    Create index.js

---
+++ Curious_Constructors/8 kyu Broken Counter/index.js
@@ -0,0 +1,23 @@
+/*
+Description:
+
+Our counter prototype is broken. Can you spot, what's wrong here?
+
+*/
+
+//My solution
+
+class Counter {
+  constructor() {
+    this.value = 0;
+  }
+  increase() {
+    this.value++;
+  }
+  getValue() {
+    return this.value;
+  }
+  reset() {
+    this.value = 0;
+  }
+}
