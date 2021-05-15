1189c451a13d35e54c25bee1c0bea6ee9104faa8
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-15 07:37:32 +0200

    Create index.js

---
+++ Strange_Strings_8kyu_1/8 kyu Define a card suit/index.js
@@ -0,0 +1,30 @@
+/*
+Description:
+
+You get any card as an argument. Your task is to return a suit of this card.
+
+Our deck (is preloaded):
+
+deck = ['2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣','A♣',
+        '2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦','A♦',
+        '2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥','A♥',
+        '2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠','A♠'];
+
+('3♣') -> return 'clubs'
+('3♦') -> return 'diamonds'
+('3♥') -> return 'hearts'
+('3♠') -> return 'spades'
+
+*/
+
+//My solution
+
+function defineSuit(card) {
+  return card.includes("♣")
+    ? "clubs"
+    : card.includes("♦")
+    ? "diamonds"
+    : card.includes("♥")
+    ? "hearts"
+    : "spades";
+}
