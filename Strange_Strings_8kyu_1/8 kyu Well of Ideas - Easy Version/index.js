1f61604f14d817e12495e8c4f0f0a3db814009a1
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-15 07:14:46 +0200

    Create index.js

---
+++ Strange_Strings_8kyu_1/8 kyu Well of Ideas - Easy Version/index.js
@@ -0,0 +1,19 @@
+/*
+Description:
+
+For every good kata idea there seem to be quite a few bad ones!
+
+In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, 
+return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
+*/
+
+//My solution
+
+function well(x) {
+  return !x.includes("good")
+    ? "Fail!"
+    : x.filter((item) => item === "good").length >= 3
+    ? "I smell a series!"
+    : "Publish!";
+}
