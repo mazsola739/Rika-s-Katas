34afb3ea15a1347cdf87398f2b1fc3ee9be26a0b
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-15 07:59:33 +0200

    Create index.js

---
+++ Diverging_Dungeons/8 kyu Drink about/index.js
@@ -0,0 +1,44 @@
+/*
+Description:
+
+    Kids drink toddy.
+    Teens drink coke.
+    Young adults drink beer.
+    Adults drink whisky.
+
+Make a function that receive age, and return what they drink.
+
+Rules:
+
+    Children under 14 old.
+    Teens under 18 old.
+    Young under 21 old.
+    Adults have 21 or more.
+
+Examples: (Input --> Output)
+
+13 --> "drink toddy"
+17 --> "drink coke"
+18 --> "drink beer"
+20 --> "drink beer"
+30 --> "drink whisky"
+
+
+*/
+
+//My solution
+
+function peopleWithAgeDrink(old) {
+  if (old < 14) {
+    return "drink toddy";
+  }
+  if (old < 18) {
+    return "drink coke";
+  }
+  if (old < 21) {
+    return "drink beer";
+  }
+  if (old >= 21) {
+    return "drink whisky";
+  }
+}
