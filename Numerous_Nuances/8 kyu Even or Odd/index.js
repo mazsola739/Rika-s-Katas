eaea23e56af3efc7553b6a7cdc3790240b248df5
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-15 09:00:59 +0200

    Create index.js

---
+++ Numerous_Nuances/8 kyu Even or Odd/index.js
@@ -0,0 +1,12 @@
+/*
+Description:
+
+Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+*/
+
+//My solution
+
+function even_or_odd(number) {
+  return number % 2 === 0 ? "Even" : "Odd";
+}
