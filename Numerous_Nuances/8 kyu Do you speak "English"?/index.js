a5fb945b6ba17eae75eb7c0e5129c6b73690ecaa
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-15 08:59:51 +0200

    Create index.js

---
+++ Numerous_Nuances/8 kyu Do you speak "English"?/index.js
@@ -0,0 +1,23 @@
+/*
+Description:
+
+Given a string of arbitrary length with any ascii characters. Write a function to determine whether the string contains the whole word "English".
+
+The order of characters is important -- a string "abcEnglishdef" is correct but "abcnEglishsef" is not correct.
+
+Upper or lower case letter does not matter -- "eNglisH" is also correct.
+
+Return value as boolean values, true for the string to contains "English", false for it does not.
+
+*/
+
+//My solution
+
+function spEng(sentence) {
+  let regex = /english/gi;
+  if (sentence.match(regex) === null) {
+    return false;
+  } else {
+    return true;
+  }
+}
