aa6d5f55137cc086fd8467e53996255b090d78cf
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-15 07:36:20 +0200

    Create index.js

---
+++ Strange_Strings_8kyu_1/8 kyu Name Shuffler/index.js
@@ -0,0 +1,14 @@
+/*
+Description:
+
+Write a function that returns a string in which firstname is swapped with last name.
+
+nameShuffler('john McClane'); => "McClane john"
+
+*/
+
+//My solution
+
+function nameShuffler(str) {
+  return str.split(" ").reverse().join(" ");
+}
