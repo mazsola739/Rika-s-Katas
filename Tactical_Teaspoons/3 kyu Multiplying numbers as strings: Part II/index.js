4cb8cca0ab287ab6056446f5803994ab5ba1ad95
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-15 10:06:51 +0200

    Create index.js

---
+++ Tactical_Teaspoons/3 kyu Multiplying numbers as strings: Part II/index.js
@@ -0,0 +1,17 @@
+/*
+This is the Part II of Multiplying numbers as strings.
+TODO
+
+Multiply two numbers! Simple!
+Rules
+
+    The arguments are passed as strings.
+    The numbers will be very large
+    The arguments can be negative, in decimals, and might have leading and trailing zeros. e.g. "-01.300"
+    Answer should be returned as string
+    The returned answer should not have leading or trailing zeroes (when applicable) e.g. "0123" and "1.100" are wrong, they should be "123" and "1.1"
+    Zero should not be signed and "-0.0" should be simply returned as "0". 
+*/
+
+//My solution
+
