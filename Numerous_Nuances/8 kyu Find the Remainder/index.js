2333e3dfc542bab3264e96ca3ccf8e249b946d7f
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-15 08:52:04 +0200

    Create index.js

---
+++ Numerous_Nuances/8 kyu Find the Remainder/index.js
@@ -0,0 +1,13 @@
+/*
+Description:
+
+Write a function that accepts two arguments and returns the remainder after dividing the larger number by the smaller number. 
+Division by zero should return NaN (in C#, throw a new DivideByZeroException instead). Arguments will both be integers.
+
+*/
+
+//My solution
+
+function remainder(a, b) {
+  return a > b ? a % b : b % a;
+}
