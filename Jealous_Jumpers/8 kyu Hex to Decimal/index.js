409f8d7c80ef0e700b7d9a9cb8517f85334363f4
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-15 19:36:34 +0200

    Create index.js

---
+++ Jealous_Jumpers/8 kyu Hex to Decimal/index.js
@@ -0,0 +1,12 @@
+/*
+Description:
+
+Complete the function which converts hex number (given as a string) to a decimal number.
+
+*/
+
+//My solution
+
+function hexToDec(hexString) {
+  return parseInt(hexString, 16);
+}
