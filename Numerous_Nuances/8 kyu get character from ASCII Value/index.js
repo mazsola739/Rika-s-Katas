b31a1737036bf8c2b5df425074a6e9ea6961792a
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-15 08:54:01 +0200

    Create index.js

---
+++ Numerous_Nuances/8 kyu get character from ASCII Value/index.js
@@ -0,0 +1,18 @@
+/*
+Description:
+
+Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+Example:
+
+get_char(65) # => 'A'
+
+For ASCII table, you can refer to http://www.asciitable.com/
+
+*/
+
+//My solution
+
+function getChar(c) {
+  return String.fromCharCode(c);
+}
