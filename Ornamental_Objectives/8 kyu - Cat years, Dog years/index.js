8cfa1b47d81c1f6c5a83e1af6f04af2982f1efa2
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-15 07:02:41 +0200

    Create index.js

---
+++ Ornamental_Objectives/8 kyu - Cat years, Dog years/index.js
@@ -0,0 +1,52 @@
+/*
+Description:
+Kata Task
+
+I have a cat and a dog.
+
+I got them at the same time as kitten/puppy. That was humanYears years ago.
+
+Return their respective ages now as [humanYears,catYears,dogYears]
+
+NOTES:
+
+    humanYears >= 1
+    humanYears are whole numbers only
+
+Cat Years
+
+    15 cat years for first year
+    +9 cat years for second year
+    +4 cat years for each year after that
+
+Dog Years
+
+    15 dog years for first year
+    +9 dog years for second year
+    +5 dog years for each year after that
+
+References
+
+    http://www.catster.com/cats-101/calculate-cat-age-in-cat-years
+    http://www.slate.com/articles/news_and_politics/explainer/2009/05/a_dogs_life.html
+
+If you liked this Kata there is another related one here
+*/
+
+//My solution
+
+var humanYearsCatYearsDogYears = function (humanYears) {
+  let catYears = 0;
+  let dogYears = 0;
+  if (humanYears === 1) {
+    catYears = 15;
+    dogYears = 15;
+  } else if (humanYears === 2) {
+    catYears = 15 + 9;
+    dogYears = 15 + 9;
+  } else if (humanYears >= 3) {
+    catYears = 15 + 9 + (humanYears - 2) * 4;
+    dogYears = 15 + 9 + (humanYears - 2) * 5;
+  }
+  return [humanYears, catYears, dogYears];
+};
