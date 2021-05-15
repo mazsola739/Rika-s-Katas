805617260601f3e9c4c01972dd71c3e0459d6efc
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-15 08:53:11 +0200

    Create index.js

---
+++ Numerous_Nuances/8 kyu ASCII Total/index.js
@@ -0,0 +1,20 @@
+/*
+Description:
+
+You'll be given a string, and have to return the sum of all characters as an int. The function should be able to handle all ASCII characters.
+
+examples:
+
+uniTotal("a") == 97 uniTotal("aaa") == 291
+
+*/
+
+//My solution
+
+function uniTotal(str) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    count += str.charCodeAt(i);
+  }
+  return count;
+}
