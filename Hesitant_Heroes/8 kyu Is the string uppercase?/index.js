89ad1290b6e8188d82fbadcbc37c382110d76276
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-15 09:27:23 +0200

    Update index.js

--- Hesitant_Heroes/8 kyu Is the string uppercase?/index.js
+++ Hesitant_Heroes/8 kyu Is the string uppercase?/index.js
@@ -12,7 +12,8 @@ Add the isUpperCase method to String to see whether the string is ALL CAPS. For 
 "ACSKLDFJSgSKLDFJSKLDFJ".isUpperCase() == false
 "ACSKLDFJSGSKLDFJSKLDFJ".isUpperCase() == true
 
-In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
+In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at 
+all is trivially considered to be in ALL CAPS.
 
 */
 
