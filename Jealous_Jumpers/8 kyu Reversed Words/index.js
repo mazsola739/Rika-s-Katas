80423f111332733a43b9b8400f45189940089074
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-15 19:23:24 +0200

    Create index.js

---
+++ Jealous_Jumpers/8 kyu Reversed Words/index.js
@@ -0,0 +1,17 @@
+/*
+Description:
+
+Complete the solution so that it reverses all of the words within the string passed in.
+
+Example:
+
+"The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+
+*/
+
+//My solution
+
+function reverseWords(str) {
+  return str.split(" ").reverse().join(" ");
+}
