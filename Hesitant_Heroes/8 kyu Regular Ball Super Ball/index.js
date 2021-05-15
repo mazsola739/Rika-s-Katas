a3b3a2d53c30009f2fd0606ef88fb6c9b1da2e64
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-15 09:28:25 +0200

    Create index.js

---
+++ Hesitant_Heroes/8 kyu Regular Ball Super Ball/index.js
@@ -0,0 +1,24 @@
+/*
+Description:
+
+Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+
+If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+ball1 = new Ball();
+ball2 = new Ball("super");
+
+ball1.ballType     //=> "regular"
+ball2.ballType     //=> "super"
+
+*/
+
+//My solution
+
+var Ball = function (ballType) {
+  if (ballType === undefined) {
+    this.ballType = "regular";
+  } else {
+    this.ballType = "super";
+  }
+};
