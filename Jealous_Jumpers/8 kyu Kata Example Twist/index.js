0102ab75eeae90978a3f224b8fccf4d806576c4b
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-15 19:33:18 +0200

    Create index.js

---
+++ Jealous_Jumpers/8 kyu Kata Example Twist/index.js
@@ -0,0 +1,14 @@
+/*
+Description:
+
+This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+Add the value "codewars" to the array websites/Websites 1,000 times.
+
+var websites = []
+
+*/
+
+//My solution
+
+var websites = new Array(1000).fill("codewars");
