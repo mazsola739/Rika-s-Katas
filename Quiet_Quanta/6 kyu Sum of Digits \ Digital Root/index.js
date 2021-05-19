9ee465599a31fbceeeb67247f9e1f7f90e3cc320
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-19 19:12:29 +0200

    Create index.js

---
+++ Quiet_Quanta/6 kyu Sum of Digits \ Digital Root/index.js
@@ -0,0 +1,32 @@
+/*
+Description:
+
+Digital root is the recursive sum of all the digits in a number.
+
+Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+Examples
+
+    16  -->  1 + 6 = 7
+   942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+*/
+
+/*
+I really like here the best solution.... Mind blowing =D
+
+function digital_root(n) {
+  return (n - 1) % 9 + 1;
+}
+
+*/
+
+//My Solution
+
+function digital_root(n){
+  let x = 0
+  String(n).split('').map(num => {
+    x += Number(num)
+  })
+  return x >= 10 ? digital_root(x) : x
+}
