c75768fc74019f8eacfb371b0a68d7f53362c5c0
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-19 19:20:44 +0200

    Create index.js

---
+++ Quiet_Quanta/6 kyu Build Tower/index.js
@@ -0,0 +1,57 @@
+/*
+Description:
+Build Tower
+
+Build Tower by the following given argument:
+number of floors (integer and always greater than 0).
+
+Tower block is represented as *
+
+    Python: return a list;
+    JavaScript: returns an Array;
+    C#: returns a string[];
+    PHP: returns an array;
+    C++: returns a vector<string>;
+    Haskell: returns a [String];
+    Ruby: returns an Array;
+    Lua: returns a Table;
+
+Have fun!
+
+for example, a tower of 3 floors looks like below
+
+[
+  '  *  ', 
+  ' *** ', 
+  '*****'
+]
+
+and a tower of 6 floors looks like below
+
+[
+  '     *     ', 
+  '    ***    ', 
+  '   *****   ', 
+  '  *******  ', 
+  ' ********* ', 
+  '***********'
+]
+
+Go challenge Build Tower Advanced once you have finished this :)
+
+*/
+
+//My Solution
+
+function towerBuilder(nFloors) {
+  let tower = [];
+  for (let i = 0; i < nFloors; i++) {
+    tower.push(
+      " ".repeat(nFloors - i - 1) +
+        "*".repeat(i * 2 + 1) +
+        " ".repeat(nFloors - i - 1)
+    );
+    //console.log (tower)
+  }
+  return tower;
+}
