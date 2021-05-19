e92b45cf932cf466af713b395cdcf8aa118828ab
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-19 19:16:08 +0200

    Create index.js

---
+++ Quiet_Quanta/6 kyu Find the odd int/index.js
@@ -0,0 +1,24 @@
+/*
+Description:
+
+Given an array of integers, find the one that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times.
+
+*/
+
+//My Solution
+
+function findOdd(A) {
+  let count = 0;
+  for (let i = 0; i < A.length; i++) {
+    for (let j = 0; j < A.length; j++) {
+      if (A[i] === A[j]) {
+        count++;
+      }
+    }
+    if (count % 2 != 0) {
+      return A[i];
+    }
+  }
+}
