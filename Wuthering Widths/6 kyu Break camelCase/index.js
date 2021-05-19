49c2476597f48bbda234a52734fdb428266991d3
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-19 19:34:52 +0200

    Create index.js

---
+++ Wuthering Widths/6 kyu Break camelCase/index.js
@@ -0,0 +1,21 @@
+/*
+Description:
+Complete the solution so that the function will break up camel casing, using a space between words.
+Example
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+*/
+
+//My Solution
+
+function solution(string) {
+  string = string.split("");
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === string[i].toUpperCase()) {
+      string[i] = ` ${string[i]}`;
+    }
+  }
+
+  return string.join("");
+}
