89ecbdb60d395a18d6d5ca7692f2797db6b4ae30
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-22 11:21:52 +0200

    Create index.js

---
+++ Hesitant_Heroes/5 kyu Where my anagrams at?/index.js
@@ -0,0 +1,39 @@
+/*
+Description:
+
+What is an anagram? Well, two words are anagrams of each other if they both contain the same letters. For example:
+
+'abba' & 'baab' == true
+
+'abba' & 'bbaa' == true
+
+'abba' & 'abbba' == false
+
+'abba' & 'abca' == false
+
+Write a function that will find all the anagrams of a word from a list. You will be given two inputs a word and an array with words. 
+You should return an array of all the anagrams or an empty array if there are none. For example:
+
+anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
+
+anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
+
+anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
+
+Algorithms
+Strings
+*/
+
+//My solution
+
+function anagrams(word, words) {
+  let result = [],
+    cloneWords = words.map((el) => el.split("").sort().join(""));
+  word = word.split("").sort().join("");
+  for (let i = 0; i < cloneWords.length; i++) {
+    if (cloneWords[i] === word) {
+      result.push(words[i]);
+    }
+  }
+  return result;
+}
