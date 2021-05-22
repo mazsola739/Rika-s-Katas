8d7d7733d1e776f29bcd42f7940853e434fc6e7e
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-22 08:03:23 +0200

    Create index.js

---
+++ Elementary_Expeditions/7 kyu Highest and Lowest/index.js
@@ -0,0 +1,26 @@
+/*
+Description:
+
+In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+Example:
+
+highAndLow("1 2 3 4 5");  // return "5 1"
+highAndLow("1 2 -3 4 5"); // return "5 -3"
+highAndLow("1 9 3 4 -5"); // return "9 -5"
+
+Notes:
+
+    All numbers are valid Int32, no need to validate them.
+    There will always be at least one number in the input string.
+    Output string must be two numbers separated by a single space, and highest number is first.
+
+
+*/
+
+//My solution
+
+function highAndLow(numbers) {
+  numbers = numbers.split(" ").sort((a, b) => b - a);
+  return `${numbers[0].toString()} ${numbers[numbers.length - 1].toString()}`;
+}
