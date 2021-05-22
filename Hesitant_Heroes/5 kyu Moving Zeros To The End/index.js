9dffb06a753c0228869ca4a8df1b4147ef946323
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-22 11:09:25 +0200

    Create index.js

---
+++ Hesitant_Heroes/5 kyu Moving Zeros To The End/index.js
@@ -0,0 +1,16 @@
+/*
+Description:
+
+Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+*/
+
+//My solution
+
+var moveZeros = function (arr) {
+  let arr2 = arr.map((el) => el).filter((el) => el === 0);
+  arr = arr.filter((el) => el !== 0);
+  return arr.concat(arr2);
+};
