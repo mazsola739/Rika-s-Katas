cecfb87d905b58bfaa11a5988917976ebf5991d9
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-22 07:57:44 +0200

    Create index.js

---
+++ Elementary_Expeditions/7 kyu Sort Numbers/index.js
@@ -0,0 +1,23 @@
+/*
+Description:
+
+Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value 
+then it should return an empty array.
+
+For example:
+
+solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+solution(null); // should return []
+
+*/
+
+//My solution
+
+function solution(nums) {
+  if (!nums) {
+    return [];
+  }
+  return nums.sort(function (a, b) {
+    return a - b;
+  });
+}
