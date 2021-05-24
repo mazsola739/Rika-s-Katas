e4328e6415ffe5722f00cd5e0feca2851967a9bc
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-24 14:41:54 +0200

    Update index.js

--- Yummy_Yetis/7 kyu Find the nth Digit of a Number/index.js
+++ Yummy_Yetis/7 kyu Find the nth Digit of a Number/index.js
@@ -22,3 +22,11 @@ findDigit(24, -8)      returns -1
 
 //My solution
 
+var findDigit = function (num, nth) {
+  if (nth <= 0) return -1;
+  let x = Math.abs(num);
+  for (let i = 1; i < nth; i++) {
+    x = Math.floor(x / 10);
+  }
+  return x % 10;
+};
