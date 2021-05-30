f14f847c7acd157b20415d91fd0b09a5b1a71fdb
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-30 22:18:29 +0200

    Create index.js

---
+++ ~Rika's favorit Katas =D~/8 kyu 5 without numbers !!/index.js
@@ -0,0 +1,22 @@
+/*Description:
+
+Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+Good luck :)
+
+*/
+//My solution
+
+function unusualFive() {
+  let five = ['','','','','']
+  return five.length
+}
+
+//funniest xD
+
+function unusualFive() {
+  return 'fucku'.length
+}
+
