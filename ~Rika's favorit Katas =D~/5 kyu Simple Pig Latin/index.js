0cab9f2a944f0c260dc1cefb47970a741000903f
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-30 23:47:21 +0200

    Create index.js

---
+++ ~Rika's favorit Katas =D~/5 kyu Simple Pig Latin/index.js
@@ -0,0 +1,26 @@
+/*
+Description:
+
+Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+Examples
+
+pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+pigIt('Hello world !');     // elloHay orldway !
+
+*/
+
+//My solution
+
+const mapper = el => el.match(/[a-z]/gim) ? `${el.substring(1)}${el.substring(0, 1)}ay` : el
+const pigIt = str => str.split(" ").map(mapper).join(" ");
+
+//old solution
+
+function pigIt(str) {
+  return str
+    .split(" ")
+    .map((el) =>
+      el.match(/[a-z]/gim) ? `${el.substring(1)}${el.substring(0, 1)}ay` : el
+    )
+    .join(" ");
+}
