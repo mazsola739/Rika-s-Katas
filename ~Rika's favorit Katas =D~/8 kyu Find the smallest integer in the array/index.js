77467c300c06f81a7cbeca35fd15df798bcfad9f
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-30 22:53:12 +0200

    Create index.js

---
+++ ~Rika's favorit Katas =D~/8 kyu Find the smallest integer in the array/index.js
@@ -0,0 +1,20 @@
+/*
+Description:
+
+Given an array of integers your solution should find the smallest integer.
+
+For example:
+
+    Given [34, 15, 88, 2] your solution will return 2
+    Given [34, -345, -1, 100] your solution will return -345
+
+You can assume, for the purpose of this kata, that the supplied array will not be empty.
+*/
+
+//My solution
+
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return args.sort(function(a, b){return a-b})[0]
+  }
+}
