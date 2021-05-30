d2c7fea75d2440cfe111a6ef9a126039c3e834bd
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-30 22:46:09 +0200

    Create index.js

---
+++ ~Rika's favorit Katas =D~/8 kyu You Can't Code Under Pressure #1/index.js
@@ -0,0 +1,18 @@
+/*
+Description:
+
+Code as fast as you can! You need to double the integer and return it.
+
+*/
+
+//My solution
+
+function doubleInteger(i) {
+  return i * 2;
+}
+
+// xDD
+
+function doubleInteger(i) {
+  return i + i - i + i - i + i - i + i - i + i - i + i - i + i - i + i - i + i - i + i - i + i - i + i - i + i - i + i - i + i - i + i
+}
