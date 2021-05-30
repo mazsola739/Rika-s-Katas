e9deb6fd056752db8de474f154cc0bd246586730
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-30 23:11:31 +0200

    Create index.js

---
+++ ~Rika's favorit Katas =D~/6 kyu Convert string to camel case/index.js
@@ -0,0 +1,23 @@
+/*
+Description:
+
+Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
+Examples
+
+"the-stealth-warrior" gets converted to "theStealthWarrior"
+"The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+
+*/
+
+//My solution
+
+function toCamelCase(str){
+  str = str.split('');
+  return str.map(function(el, i){
+    if(el == '-' || el == '_'){
+      el = str[i+1].toUpperCase();
+      str.splice(i+1, 1);
+    }
+    return el;
+  }).join('');
+}
