a6a265d6063f93fdd045704cb6cb47f02ee4029b
Rika <60824200+mazsola739@users.noreply.github.com>
2021-05-30 23:18:33 +0200

    Create index.js

---
+++ ~Rika's favorit Katas =D~/4 kyu Sum Strings as Numbers/index.js
@@ -0,0 +1,20 @@
+/*
+Description:
+
+Given the string representations of two integers, return the string representation of the sum of those integers.
+
+For example:
+
+sumStrings('1','2') // => '3'
+
+A string representation of an integer will contain no characters besides the ten numerals "0" to "9".
+
+*/
+
+//My solution
+
+function sumStrings(a,b) { 
+  a= BigInt(a)
+  b=BigInt(b)
+  return `${a + b}`
+}
