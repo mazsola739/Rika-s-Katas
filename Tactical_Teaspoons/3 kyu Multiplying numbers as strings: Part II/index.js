efc38c4201754eacf29c2874475d11ee52ee2d35
Rika <60824200+mazsola739@users.noreply.github.com>
2021-06-03 21:14:23 +0200

    Update index.js

--- Tactical_Teaspoons/3 kyu Multiplying numbers as strings: Part II/index.js
+++ Tactical_Teaspoons/3 kyu Multiplying numbers as strings: Part II/index.js
@@ -15,3 +15,79 @@ Rules
 
 //My solution
 
+var ZERO = BigInt(0);
+var TEN = BigInt(10);
+var isInteger = (numberAsString) => numberAsString.indexOf(".") < 0;
+var bothAreIntegers = (aa, bb) => isInteger(aa) && isInteger(bb);
+
+var extractNumberToParts = (numberAsString) => {
+  //case integer
+  if (isInteger(numberAsString)) {
+    return {
+      integerOnPower: BigInt(numberAsString),
+      amountOfDecimals: ZERO
+    };
+  }
+  //case float
+
+  /*
+   * In case of positive numbers
+   * integerOnPower = (integer * (10 ** amountOfDecimals)) + (+decimals);
+   *
+   * In case of positive numbers
+   * integerOnPower = (integer * (10 ** amountOfDecimals)) + (-decimals);
+   */
+
+  const integer = BigInt(`${numberAsString}`.split(".")[0]);
+  const decimals = BigInt(`${numberAsString}`.split(".")[1]);
+  const amountOfDecimals = BigInt(
+    `${numberAsString}`.split(".")[1].length || 0
+  );
+  const addOrDistractDecimals = (base) =>
+    integer < 0 ? base - decimals : base + decimals;
+  const integerOnPower = addOrDistractDecimals(
+    integer * TEN ** amountOfDecimals
+  );
+
+  return {
+    integerOnPower,
+    amountOfDecimals
+  };
+};
+
+var multiply = (aa, bb) => {
+  if (bothAreIntegers(aa, bb)) return `${BigInt(aa) * BigInt(bb)}`;
+
+  const {
+    integerOnPower: intA,
+    amountOfDecimals: decAmountA
+  } = extractNumberToParts(aa);
+  const {
+    integerOnPower: intB,
+    amountOfDecimals: decAmountB
+  } = extractNumberToParts(bb);
+  const resultPart01 = intA * intB;
+  const resultPart02 = TEN ** (decAmountA + decAmountB);
+  const resultOnlyInt = resultPart01 / resultPart02;
+  const amountOfDecimals = `${resultPart02}`.length - 1;
+  const amountOfLeadingZeros =
+    amountOfDecimals > `${resultPart01}`.length
+      ? amountOfDecimals - `${resultPart01}`.length
+      : 0;
+  let resultOnlyFloat =
+    "0".repeat(amountOfLeadingZeros) +
+    `${resultPart01}`.substr(-amountOfDecimals);
+  for (let i = resultOnlyFloat.length; i >= 0; i--) {
+    if (resultOnlyFloat[i - 1] === "0") {
+      resultOnlyFloat = resultOnlyFloat.substr(0, resultOnlyFloat.length - 1);
+      continue;
+    }
+    break;
+  }
+
+  return !resultOnlyFloat
+    ? `${resultOnlyInt}`
+    : `${resultOnlyInt}.${resultOnlyFloat}`;
+};
+
+//done with sylwolveryn
