55d67a18128ba2f260b0c2573cad06278d27cf01
Rika <60824200+mazsola739@users.noreply.github.com>
2021-06-03 21:26:07 +0200

    Create index.js

---
+++ Utmost_Umbrellas/5 kyu Can you get the loop ?/index.js
@@ -0,0 +1,19 @@
+/*
+You are given a node that is the beginning of a linked list. This list always contains a tail and a loop. Your objective is to determine the length of the loop.
+
+For example in the following picture the tail's size is 3 and the loop size is 12:
+
+// Use the `getNext' method or 'next' property to get the following node.
+node.getNext()
+node.next
+
+Note: do NOT mutate the nodes!
+
+    Thanks to shadchnev, I broke all of the methods from the Hash class.
+
+    Don't miss dmitry's article in the discussion after you pass the Kata !!
+
+
+*/
+
+//My solution
