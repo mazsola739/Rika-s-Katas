aef538210b7e9a884d62561d2508ee95e2f887aa
Rika <60824200+mazsola739@users.noreply.github.com>
2021-06-03 20:50:24 +0200

    Update index.js

--- Interesting_Ideas/5 kyu Vector class/index.js
+++ Interesting_Ideas/5 kyu Vector class/index.js
@@ -24,3 +24,47 @@ Note: the test cases will utilize the user-provided equals method.
 
 //My solution
 
+var Vector = function (components) {
+  this.x = components;
+};
+
+Vector.prototype.add = function (b) {
+  var a = this.x;
+  b = b.x;
+  if (a.length !== b.length)
+    throw new TypeError("Vectors have different length");
+  return new Vector(
+    a.map((x, i) => x + b[i]));
+};
+
+Vector.prototype.subtract = function (b) {
+  var a = this.x;
+  b = b.x;
+  if (a.length !== b.length)
+    throw new TypeError("Vectors have different length");
+  return new Vector(a.map((x, i)=> x - b[i]));
+};
+
+Vector.prototype.dot = function (b) {
+  var a = this.x;
+  b = b.x;
+  if (a.length !== b.length)
+    throw new TypeError("Vectors have different length");
+  return a.reduce((s, x, i) => (s + x * b[i]), 0);
+};
+Vector.prototype.equals = function (b) {
+  var a = this.x;
+  b = b.x;
+  if (a.length !== b.length) return false;
+  return a.every((x, i) => x === b[i]);
+};
+
+Vector.prototype.norm = function () {
+  var a = this.x;
+  return Math.sqrt(
+    a.reduce((s, x) => s + x * x, 0));
+};
+
+Vector.prototype.toString = function () {
+  return "(" + this.x.join(",") + ")";
+};
